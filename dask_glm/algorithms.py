54bbd9fca2ad6a5cbfb5c43ca30aec430066f40c
Nick Pentreath <nickp@za.ibm.com>
2017-05-07 13:04:53 +0200

    Make regularizer a keyword arg

--- dask_glm/algorithms.py
+++ dask_glm/algorithms.py
@@ -243,14 +243,15 @@ def shrinkage(x, kappa):
     return z
 
 
-def lbfgs(X, y, max_iter=100, tol=1e-4, alpha=1.0, family=Logistic, verbose=False):
+def lbfgs(X, y, regularizer='l1', alpha=1.0, max_iter=100, tol=1e-4,
+          family=Logistic, verbose=False):
     """L-BFGS solver using scipy.optimize implementation
     
     Currently supports L2 regularization only"""
 
     pointwise_loss = family.pointwise_loss
     pointwise_gradient = family.pointwise_gradient
-    regularizer = Regularizer.get('l2')
+    regularizer = Regularizer.get(regularizer)
 
     n, p = X.shape
     beta0 = np.zeros(p)
