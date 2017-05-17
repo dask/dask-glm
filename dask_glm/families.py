67c591d443360850119d26109ace0f4bb0b5b793
Richard Postelnik <postelrich@gmail.com>
2017-05-16 21:03:07 -0400

    move pointwise functions to base.

--- dask_glm/families.py
+++ dask_glm/families.py
@@ -10,14 +10,20 @@ class Family(object):
     with the algorithms.
     """
 
-    def loglike(self, Xbeta, y):
+    def loglikelihood(self, Xbeta, y):
         raise NotImplementedError
 
     def pointwise_loss(self, beta, X, y):
-        raise NotImplementedError
+        """Loss, evaluated point-wise."""
+        beta, y = beta.ravel(), y.ravel()
+        Xbeta = X.dot(beta)
+        return self.loglikelihood(Xbeta, y)
 
     def pointwise_gradient(self, beta, X, y):
-        raise NotImplementedError
+        """Loss, evaluated point-wise."""
+        beta, y = beta.ravel(), y.ravel()
+        Xbeta = X.dot(beta)
+        return self.gradient(Xbeta, X, y)
 
     def gradient(self, Xbeta, x, y):
         raise NotImplementedError
@@ -26,16 +32,16 @@ class Family(object):
         raise NotImplementedError
 
 
-class Logistic(object):
+class Logistic(Family):
     """Implements methods for `Logistic regression`_,
     useful for classifying binary outcomes.
 
     .. _Logistic regression: https://en.wikipedia.org/wiki/Logistic_regression
     """
-    @staticmethod
-    def loglike(Xbeta, y):
+
+    def loglikelihood(self, Xbeta, y):
         """
-        Evaluate the logistic loglikeliehood
+        Evaluate the logistic loglikelihoodliehood
 
         Parameters
         ----------
@@ -45,61 +51,31 @@ class Logistic(object):
         enXbeta = exp(-Xbeta)
         return (Xbeta + log1p(enXbeta)).sum() - dot(y, Xbeta)
 
-    @staticmethod
-    def pointwise_loss(beta, X, y):
-        """Logistic Loss, evaluated point-wise."""
-        beta, y = beta.ravel(), y.ravel()
-        Xbeta = X.dot(beta)
-        return Logistic.loglike(Xbeta, y)
-
-    @staticmethod
-    def pointwise_gradient(beta, X, y):
-        """Logistic gradient, evaluated point-wise."""
-        beta, y = beta.ravel(), y.ravel()
-        Xbeta = X.dot(beta)
-        return Logistic.gradient(Xbeta, X, y)
-
-    @staticmethod
-    def gradient(Xbeta, X, y):
+    def gradient(self, Xbeta, X, y):
         """Logistic gradient"""
         p = sigmoid(Xbeta)
         return dot(X.T, p - y)
 
-    @staticmethod
-    def hessian(Xbeta, X):
+    def hessian(self, Xbeta, X):
         """Logistic hessian"""
         p = sigmoid(Xbeta)
         return dot(p * (1 - p) * X.T, X)
 
 
-class Normal(object):
+class Normal(Family):
     """Implements methods for `Linear regression`_,
     useful for modeling continuous outcomes.
 
     .. _Linear regression: https://en.wikipedia.org/wiki/Linear_regression
     """
-    @staticmethod
-    def loglike(Xbeta, y):
+
+    def loglikelihood(self, Xbeta, y):
         return ((y - Xbeta) ** 2).sum()
 
-    @staticmethod
-    def pointwise_loss(beta, X, y):
-        beta, y = beta.ravel(), y.ravel()
-        Xbeta = X.dot(beta)
-        return Normal.loglike(Xbeta, y)
-
-    @staticmethod
-    def pointwise_gradient(beta, X, y):
-        beta, y = beta.ravel(), y.ravel()
-        Xbeta = X.dot(beta)
-        return Normal.gradient(Xbeta, X, y)
-
-    @staticmethod
-    def gradient(Xbeta, X, y):
+    def gradient(self, Xbeta, X, y):
         return 2 * dot(X.T, Xbeta) - 2 * dot(X.T, y)
 
-    @staticmethod
-    def hessian(Xbeta, X):
+    def hessian(self, Xbeta, X):
         return 2 * dot(X.T, X)
 
 
@@ -112,31 +88,16 @@ class Poisson(object):
     .. _Poisson regression: https://en.wikipedia.org/wiki/Poisson_regression
     """
 
-    @staticmethod
-    def loglike(Xbeta, y):
+    def loglikelihood(self, Xbeta, y):
         eXbeta = exp(Xbeta)
         yXbeta = y * Xbeta
         return (eXbeta - yXbeta).sum()
 
-    @staticmethod
-    def pointwise_loss(beta, X, y):
-        beta, y = beta.ravel(), y.ravel()
-        Xbeta = X.dot(beta)
-        return Poisson.loglike(Xbeta, y)
-
-    @staticmethod
-    def pointwise_gradient(beta, X, y):
-        beta, y = beta.ravel(), y.ravel()
-        Xbeta = X.dot(beta)
-        return Poisson.gradient(Xbeta, X, y)
-
-    @staticmethod
-    def gradient(Xbeta, X, y):
+    def gradient(self, Xbeta, X, y):
         eXbeta = exp(Xbeta)
         return dot(X.T, eXbeta - y)
 
-    @staticmethod
-    def hessian(Xbeta, X):
+    def hessian(self, Xbeta, X):
         eXbeta = exp(Xbeta)
         x_diag_eXbeta = eXbeta[:, None] * X
         return dot(X.T, x_diag_eXbeta)
