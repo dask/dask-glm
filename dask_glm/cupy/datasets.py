9127afa0be2bae95a4128114c58ab6c99f005657
Peter Andreas Entschev <peter@entschev.com>
2019-03-05 17:39:01 +0100

    Add CuPy datasets

---
+++ dask_glm/cupy/datasets.py
@@ -0,0 +1,114 @@
+import numpy as np
+import cupy
+
+
+def make_classification(n_samples=1000, n_features=100, n_informative=2, scale=1.0):
+    """
+    Generate a dummy dataset for classification tasks.
+
+    Parameters
+    ----------
+    n_samples : int
+        number of rows in the output array
+    n_features : int
+        number of columns (features) in the output array
+    n_informative : int
+        number of features that are correlated with the outcome
+    scale : float
+        Scale the true coefficient array by this
+
+    Returns
+    -------
+    X : dask.array, size ``(n_samples, n_features)``
+    y : dask.array, size ``(n_samples,)``
+        boolean-valued array
+
+    Examples
+    --------
+    >>> X, y = make_classification()
+    >>> X.dtype, X.shape
+    (dtype('float64'), (1000, 100))
+    >>> y.dtype, y.shape
+    (dtype('bool'), (1000,))
+    """
+    X = cupy.random.normal(0, 1, size=(n_samples, n_features))
+    informative_idx = np.random.choice(n_features, n_informative)
+    beta = (cupy.random.random(n_features) - 1) * scale
+    z0 = X[:, informative_idx].dot(beta[informative_idx])
+    y = cupy.random.random(z0.shape) < 1 / (1 + np.exp(-z0))
+    return X, y
+
+
+def make_regression(n_samples=1000, n_features=100, n_informative=2, scale=1.0):
+    """
+    Generate a dummy dataset for regression tasks.
+
+    Parameters
+    ----------
+    n_samples : int
+        number of rows in the output array
+    n_features : int
+        number of columns (features) in the output array
+    n_informative : int
+        number of features that are correlated with the outcome
+    scale : float
+        Scale the true coefficient array by this
+
+    Returns
+    -------
+    X : dask.array, size ``(n_samples, n_features)``
+    y : dask.array, size ``(n_samples,)``
+        real-valued array
+
+    Examples
+    --------
+    >>> X, y = make_regression()
+    >>> X.dtype, X.shape
+    (dtype('float64'), (1000, 100))
+    >>> y.dtype, y.shape
+    (dtype('float64'), (1000,))
+    """
+    X = cupy.random.normal(0, 1, size=(n_samples, n_features))
+    informative_idx = cupy.random.choice(n_features, n_informative)
+    beta = (cupy.random.random(n_features) - 1) * scale
+    z0 = X[:, informative_idx].dot(beta[informative_idx])
+    y = cupy.random.random(z0.shape)
+    return X, y
+
+
+def make_poisson(n_samples=1000, n_features=100, n_informative=2, scale=1.0):
+    """
+    Generate a dummy dataset for modeling count data.
+
+    Parameters
+    ----------
+    n_samples : int
+        number of rows in the output array
+    n_features : int
+        number of columns (features) in the output array
+    n_informative : int
+        number of features that are correlated with the outcome
+    scale : float
+        Scale the true coefficient array by this
+
+    Returns
+    -------
+    X : dask.array, size ``(n_samples, n_features)``
+    y : dask.array, size ``(n_samples,)``
+        array of non-negative integer-valued data
+
+    Examples
+    --------
+    >>> X, y = make_poisson()
+    >>> X.dtype, X.shape 
+    (dtype('float64'), (1000, 100))
+    >>> y.dtype, y.shape
+    (dtype('int64'), (1000,))
+    """
+    X = cupy.random.normal(0, 1, size=(n_samples, n_features))
+    informative_idx = cupy.random.choice(n_features, n_informative)
+    beta = (cupy.random.random(n_features) - 1) * scale
+    z0 = X[:, informative_idx].dot(beta[informative_idx])
+    rate = np.exp(z0)
+    y = cupy.random.poisson(rate)
+    return X, y
